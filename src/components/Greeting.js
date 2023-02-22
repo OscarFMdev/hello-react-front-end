bd4ad23036e0a62140c95e70c1047650938f44a2
Oscar Fernández Muñoz <oscarfmdev@gmail.com>
2023-02-22 17:19:37 -0600

    Configure linters and fix linter errors

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,10 +4,10 @@ import { useSelector } from 'react-redux';
 function Greeting() {
   const greeting = useSelector((state) => state.greeting.message.greeting);
   const status = useSelector((state) => state.greeting.status);
-  
+
   if (status === 'succeeded') {
     return (
-        <>
+      <>
         <h1>From API:</h1>
         <p className=".App-link">{ greeting }</p>
       </>
